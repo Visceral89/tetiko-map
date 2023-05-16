79eb400882e42a0afc6a88a67d6c4d41d127e304
Rasmus Bremholm <rasmus.brem@gmail.com>
2023-05-16 22:45:36 +0200

    Three js setup

--- app/page.js
+++ app/page.js
@@ -1,33 +1,8 @@
-"use client";
+
 
 import Image from "next/image";
 import styles from "@/styles/page.module.scss";
-import SignIn from "@/components/SignIn.jsx";
-import supabase from "../utils/supabaseClient.js";
-import { useEffect, useState } from "react";
 
 export default function Home() {
-	const [user, setUser] = useState(null);
-
-	useEffect(() => {
-		const sessionUser = supabase.auth.user();
-
-		setUser(sessionUser);
-
-		const { data: authListener } = supabase.auth.onAuthStateChange(
-			(event, session) => {
-				const currentUser = session ? session.user : null;
-				setUser(currentUser);
-			}
-		);
-		return () => {
-			authListener.unsubscribe();
-		};
-	}, []);
-
-	if (!user) {
-		return <SignIn />;
-	}
-
 	return <main className={styles.main}>Hello</main>;
 }
---
+++ components/Scene.jsx
@@ -0,0 +1,96 @@
+import { useEffect, useRef } from "react";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+export default function Scene() {
+	const container = useRef();
+
+	const width = window.innerWidth;
+	const height = window.innerHeight;
+
+	useEffect(() => {
+		const scene = new THREE.Scene();
+
+		// Camera Variables
+
+		const fov = 90;
+		const nearclip = 0.1;
+		const farclip = 1000;
+
+		const camera = new THREE.PerspectiveCamera(
+			fov,
+			width / height,
+			nearclip,
+			farclip
+		);
+		camera.position.z = 5;
+
+		// Rendering Variables
+		const b_antialias = true;
+
+		const renderer = new THREE.WebGLRenderer({ antialias: b_antialias });
+		renderer.setSize(width, height);
+		container.current.appendChild(renderer.domElement);
+
+		// Resize Window
+		window.addEventListener("resize", function () {
+			const width = window.innerWidth;
+			const height = window.innerHeight;
+			renderer.setSize(width, height);
+			camera.aspect = width / height;
+			camera.updateProjectionMatrix();
+		});
+
+		// GLTF Loader
+		const loader = new GLTFLoader();
+
+		function loadModel(url) {
+			return new Promise((resolve, reject) => {
+				loader.load(
+					url,
+					(gltf) => {
+						scene.add(gltf.scene);
+						resolve(gltf);
+					},
+					undefined,
+					(error) => {
+						console.error("Failed to load model", error);
+						reject(error);
+					}
+				);
+			});
+		}
+
+		const modelUrls = [
+			"@/models/model1.gltf",
+			"@/models/model2.gltf",
+			"@/models/model3.gltf",
+		];
+		Promise.all(modelUrls.map(loadModel))
+			.then(() => {
+				console.log("All models loaded");
+			})
+			.catch((error) => {
+				console.error("An error happened while loading models", error);
+			});
+
+		function animate() {
+			requestAnimationFrame(animate);
+			renderer.render(scene, camera);
+
+			// Update controls!
+			const controls = new OrbitControls(camera, renderer.domElement);
+			controls.update();
+		}
+
+		animate();
+
+		return () => {
+			window.removeEventListener("resize", function () {});
+			container.current.removeChild(renderer.domElement);
+		};
+	}, []);
+
+	return <div ref={container}></div>;
+}
