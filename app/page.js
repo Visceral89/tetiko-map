467f8a39584df027b5d19996e8faaae6fa95cfc9
Rasmus Bremholm <rasmus.brem@gmail.com>
2023-05-16 16:05:34 +0200

    supabase.js

--- app/page.js
+++ app/page.js
@@ -2,6 +2,17 @@ import Image from "next/image";
 import styles from "@/styles/page.module.scss";
 import Login from "@/components/Login";
 
+import supabase from "@/utils/supabase.js";
+
+async function fetchData() {
+	let { data: users, error } = await supabase.from("users").select("*");
+
+	if (error) console.log("Error: ", error);
+	else console.log("Users: ", users);
+}
+
+fetchData();
+
 export default function Home() {
 	return (
 		<main className={styles.main}>
