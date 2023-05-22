95f675897526d1a4ef2c2fc518d387ac6e59c8e5
Rasmus Bremholm <rasmus.brem@gmail.com>
2023-05-22 22:41:46 +0200

    added an image

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,17 @@
 import styles from "../styles/navbar.module.scss";
+import Image from "next/image";
 import SignInBtn from "./SignInBtn";
 import SignOutBtn from "./SignOutBtn";
 
 const Navbar = () => {
 	return (
 		<nav className={styles.navbar}>
+			<Image
+				scr="/logos/tetiko.png"
+				alt="tetiko logo"
+				width={372}
+				height={121}
+			/>
 			<div className={styles.navbarItem}>Tetiko Platskarta</div>
 			<div>
 				<SignInBtn />
