7d7c693bce853e0373c21bbd3d9c19005b8cd920
Rasmus Bremholm <rasmus.brem@gmail.com>
2023-05-23 15:44:25 +0200

    login button icon

--- components/SignInBtn.jsx
+++ components/SignInBtn.jsx
@@ -1,7 +1,18 @@
 import styles from "../styles/button.module.scss";
+import Image from "next/image";
 
 const SignInBtn = () => {
-	return <button className={styles.btn}>Login</button>;
+	return (
+		<button className={styles.btn}>
+			<Image
+				src="/icons/login_black_24dp.svg"
+				width={20}
+				height={20}
+				alt="login icons"
+			/>
+			Login
+		</button>
+	);
 };
 
 export default SignInBtn;
